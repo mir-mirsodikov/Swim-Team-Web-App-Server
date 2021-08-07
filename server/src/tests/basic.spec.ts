9a4c0a908bac77ce643ada441377b98b8c090d74
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2021-08-07 09:40:05 -0500

    Add CI to GitHub

---
+++ server/src/tests/basic.spec.ts
@@ -0,0 +1,10 @@
+import 'mocha';
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('Basic testing with Mocha/Chai', () => {
+  it('Are Mocha and Chai correctly set up?', () => {
+    expect(true).to.be.true;
+  });
+});
