8003f64ad4ea290d309b09ecacf3c504aa0f6208
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2021-08-09 23:21:01 -0500

    Removed DB provider files. They did not seem like they served any purpose so far.

--- server/src/infrastructure/MemoryDB.ts
+++
@@ -1,9 +0,0 @@
-import { MongoSchemas } from '@db/interfaces';
-
-export abstract class MemoryDB<T> {
-  constructor(private readonly dbSchemas: MongoSchemas) {
-    this.setup();
-  }
-
-  abstract setup(): void;
-}
--- server/src/infrastructure/interfaces.d.ts
+++
@@ -1,5 +0,0 @@
-import { IAnnouncementGateway } from '@domain/interfaces';
-
-export interface IGatewayProvider {
-  announcement: IAnnouncementGateway
-}
